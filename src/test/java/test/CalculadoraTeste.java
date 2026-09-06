a33689c6a79a8ff58be6d8f3bf8a5c16c2e27c1e
Gabriel9706374 <gabriel9706374@rede.ulbra.br>
2026-09-06 19:13:50 -0300

    Add files via upload

--- src/test/java/test/CalculadoraTeste.java
+++ src/test/java/test/CalculadoraTeste.java
@@ -5,11 +5,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTeste {
-    
+
     @Test
-    public void testSomaDoisNumerosPositivos(){
+    public void testSomaDoisNumerosPositivos() {
         Calculadora calculadora = new Calculadora();
-        calculadora.somar(5,3);
+
+        calculadora.somar(5, 3);
+
         assertEquals(8, calculadora.resultado);
-    }  
+    }
+
+    @Test
+    public void testSubtracaoDoisNumeros() {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.substrair(10, 4);
+
+        assertEquals(6, calculadora.resultado);
+    }
+
+    @Test
+    public void testMultiplicacaoDoisNumeros() {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.multiplicar(5, 4);
+
+        assertEquals(20, calculadora.resultado);
+    }
+
+    @Test
+    public void testDivisaoDoisNumeros() {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.dividir(10, 2);
+
+        assertEquals(5, calculadora.resultado);
+    }
 }
